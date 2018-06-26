20ff127323210dd6ec61225bcf2d2534af943906
John Rekoske <jrekoske@usgs.gov>
2018-06-26 11:54:23 -0600

    Modified tests so that the entire array of processed data would be checked.

--- tests/amptools/process_test.py
+++ tests/amptools/process_test.py
@@ -9,20 +9,20 @@ from obspy.core.stream import read
 from obspy.core.utcdatetime import UTCDateTime
 from amptools import process
 
+homedir = os.path.dirname(os.path.abspath(__file__))
+datadir = os.path.join(homedir, '..', 'data', 'process')
+
 
 def test_amp_check_trim():
-    homedir = os.path.dirname(os.path.abspath(__file__))
 
     # read the two sac files for testing
     # one is unedited with a standard maximum amplitude
     # the second has been multiplied so that it fails the ampltidue check
-    NOWS_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                                'NOWSENR.sac'))[0]
-    NOWS_tr_mul = read(os.path.join(homedir, '..', 'data', 'process',
-                                    'NOWSENR_mul.sac'))[0]
+    NOWS_tr = read(os.path.join(datadir, 'NOWSENR.sac'))[0]
+    NOWS_tr_mul = read(os.path.join(datadir, 'NOWSENR_mul.sac'))[0]
 
-    assert process.check_max_amplitude(NOWS_tr) == True
-    assert process.check_max_amplitude(NOWS_tr_mul) == False
+    assert process.check_max_amplitude(NOWS_tr) is True
+    assert process.check_max_amplitude(NOWS_tr_mul) is False
 
     # Check that our trim and window function doesn't affect the ending time
     # of this record
@@ -32,15 +32,11 @@ def test_amp_check_trim():
 
 
 def test_corner_freqs():
-    homedir = os.path.dirname(os.path.abspath(__file__))
     event_time = UTCDateTime('2001-02-28T18:54:32')
-
-    ALCT_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                                'ALCTENE.UW..sac'))[0]
+    ALCT_tr = read(os.path.join(datadir, 'ALCTENE.UW..sac'))[0]
     ALCT_dist = 75.9559
 
-    GNW_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                               'GNWBHE.UW..sac'))[0]
+    GNW_tr = read(os.path.join(datadir, 'GNWBHE.UW..sac'))[0]
     GNW_dist = 46.7473
 
     corners_1 = process.get_corner_frequencies(ALCT_tr, event_time, ALCT_dist)
@@ -50,8 +46,7 @@ def test_corner_freqs():
     np.testing.assert_allclose(corners_2, [0.03662, 25.0], atol=0.001)
 
     event_time = UTCDateTime('2016-10-22T17:17:05')
-    ALKI_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                                'ALKIENE.UW..sac'))[0]
+    ALKI_tr = read(os.path.join(datadir, 'ALKIENE.UW..sac'))[0]
     ALKI_dist = 37.87883
 
     corners_3 = process.get_corner_frequencies(ALKI_tr, event_time, ALKI_dist)
@@ -59,50 +54,53 @@ def test_corner_freqs():
 
 
 def test_all():
-    homedir = os.path.dirname(os.path.abspath(__file__))
-    event_time = UTCDateTime('2001-02-28T18:54:32')
-    ALCT_dist = 75.9559
-    ALCT_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                                'ALCTENE.UW..sac'))[0]
+    # Get our data directory
+    event_time = UTCDateTime('2003-01-15T03:41:58')
+    HAWA_dist = 80.1893
+    HAWA_tr = read(os.path.join(datadir, 'HAWABHN.US..sac'))[0]
+    HAWA_processed = process.process_all(HAWA_tr, event_time, HAWA_dist)
 
-    GNW_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                               'GNWBHE.UW..sac'))[0]
-    GNW_dist = 46.7473
-    GNW_processed = process.rennolet_process(GNW_tr, event_time, GNW_dist)
-    last_point = GNW_processed.data[GNW_processed.stats.npts-1]
-    np.testing.assert_allclose(last_point, 0, atol=0.0001)
+    # Load in the already calculated array form processing
+    HAWA_array = np.genfromtxt(os.path.join(datadir,
+                                            'HAWABHN.US..sac.acc.final.txt'),
+                               dtype=float)
+    HAWA_array = HAWA_array.T
+    HAWA_calc_data = HAWA_array[1]
 
-    # Triggers the invalid low pass filtering message
-    ALCT_processed = process.rennolet_process(ALCT_tr, event_time, ALCT_dist)
-    last_point = ALCT_processed.data[ALCT_processed.stats.npts-1]
-    np.testing.assert_allclose(last_point, 0, atol=0.0001)
-
-    # Test trace with invalid amplitudes
-    NOWS_tr_mul = read(os.path.join(homedir, '..', 'data', 'process',
-                                    'NOWSENR_mul.sac'))[0]
-    time = UTCDateTime('2001-02-14T22:03:58')
-    dist = 50.05
-    assert process.rennolet_process(NOWS_tr_mul, time, dist) == NOWS_tr_mul
-
-    # Test trace with low S/N ratio
-    event_time = UTCDateTime('2016-10-22T17:17:05')
-    ALKI_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                                'ALKIENE.UW..sac'))[0]
-    ALKI_dist = 37.87883
-    ALKI_processed = process.rennolet_process(ALKI_tr, event_time, ALKI_dist)
-    assert ALKI_processed == ALKI_tr
+    # Compare the processing script with the already processed data
+    np.testing.assert_allclose(HAWA_processed.data, HAWA_calc_data, atol=0.001)
 
     # Test file that will conduct low-pass filter
     event_time = UTCDateTime('2001-02-28T18:54:32')
     BRI_dist = 55.6385
-    BRI_tr = read(os.path.join(homedir, '..', 'data', 'process',
-                               'BRIHN1.GS..sac'))[0]
-    BRI_processed = process.rennolet_process(BRI_tr, event_time, BRI_dist)
-    last_point = BRI_processed.data[BRI_processed.stats.npts-1]
-    np.testing.assert_allclose(last_point, 0, atol=0.0001)
+    BRI_tr = read(os.path.join(datadir, 'BRIHN1.GS..sac'))[0]
+    process.process_all(BRI_tr, event_time, BRI_dist)
+
+    # Triggers the invalid low pass filtering message
+    event_time = UTCDateTime('2001-02-28T18:54:32')
+    ALCT_dist = 75.9559
+    ALCT_tr = read(os.path.join(datadir, 'ALCTENE.UW..sac'))[0]
+    process.process_all(ALCT_tr, event_time, ALCT_dist)
+
+    GNW_tr = read(os.path.join(datadir, 'GNWBHE.UW..sac'))[0]
+    GNW_dist = 46.7473
+    process.process_all(GNW_tr, event_time, GNW_dist)
+
+    # Test trace with invalid amplitudes
+    NOWS_tr_mul = read(os.path.join(datadir, 'NOWSENR_mul.sac'))[0]
+    time = UTCDateTime('2001-02-14T22:03:58')
+    dist = 50.05
+    assert process.process_all(NOWS_tr_mul, time, dist) == NOWS_tr_mul
+
+    # Test trace with low S/N ratio
+    event_time = UTCDateTime('2016-10-22T17:17:05')
+    ALKI_tr = read(os.path.join(datadir, 'ALKIENE.UW..sac'))[0]
+    ALKI_dist = 37.87883
+    ALKI_processed = process.process_all(ALKI_tr, event_time, ALKI_dist)
+    assert ALKI_processed == ALKI_tr
 
 
 if __name__ == '__main__':
-    test_amp_check_trim()
-    test_corner_freqs()
+    # test_amp_check_trim()
+    # test_corner_freqs()
     test_all()
