620dbc08f13bbf00ac0279af9399c4d8b285a58e
John Rekoske <jrekoske@usgs.gov>
2018-11-05 18:09:46 -0700

    adding data

--- tests/bin/fdsnfetch_test.py
+++ tests/bin/fdsnfetch_test.py
@@ -41,15 +41,23 @@ def test_fdsnfetch():
     cmd_input = '%s %s' % (datadir, parameters)
     cmd = '%s %s' % (fdsnfetch, cmd_input)
     res, stdout, stderr = get_command_output(cmd)
-    print(stdout.decode('utf-8').strip())
-    print(stderr.decode('utf-8').strip())
 
     # Confirm that we got the three ALCT files as expected
-    st, inv = request_raw_waveforms('IRIS', '2001-02-28T18:54:32', 47.149,
-                                    -122.7266667, before_time=120,
-                                    after_time=120, dist_max=1.0,
-                                    stations=['ALCT'], channels=['EN*'])
-    assert len(st) == 3
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..ENE.MSEED')
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..ENN.MSEED')
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..ENZ.MSEED')
+
+    # Peform another request, this time modifying the channel names
+    parameters = 'IRIS 2001-02-28T18:54:32 47.149 -122.7266667 -dmax 1.0'
+    parameters += ' -n UW -s ALCT -c EN* -r -m'
+    cmd_input = '%s %s' % (datadir, parameters)
+    cmd = '%s %s' % (fdsnfetch, cmd_input)
+    res, stdout, stderr = get_command_output(cmd)
+
+    # Confirm that the channel names were appropriately modified
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..HN1.MSEED')
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..HN2.MSEED')
+    assert os.path.isfile(datadir + '/raw/UW.ALCT..HNZ.MSEED')
 
 
 if __name__ == '__main__':
